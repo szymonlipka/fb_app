ce7dd4c261a754a0553c73483b84d3710fd8bcd4
szymonlipka <revan12311@gmail.com>
2015-12-28 17:02:03 +0100

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module TestApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
     config.generators do |g| 
       g.template_engine :haml
