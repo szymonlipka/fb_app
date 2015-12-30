450caf99824d5ab2e1734496086a1622ba0fbaf3
szymonlipka <revan12311@gmail.com>
2015-12-30 21:41:06 +0100

    Method listing posts on main manu

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :invitations
   validates :username, presence: true
+
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
@@ -17,6 +18,7 @@ class User < ActiveRecord::Base
       end
     end
   end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -24,25 +26,21 @@ class User < ActiveRecord::Base
       user.username = auth.info.name
     end
   end
+
   def accept_invite(invitation)
     group = Group.find(invitation.group_id)
-    (!self.groups.include? group) ? groups << group : false
+    groups << group unless groups.include?(group)
     invitation.delete
   end
+
   def decline_invite(invitation)
     invitation.delete
   end
+
   def list_posts
-    list = []
-    groups.each do |group|
-      group.posts.each do |post|
-        list << post
-      end
-    end
-    posts.each do |post|
-      list << post if !list.include? post
-    end
-    list.sort_by!(&:created_at)
-    list.reverse
+    group_posts = Post.where(group_id: groups.pluck(:id)).where_values.reduce(:and)
+    my_posts = posts.where_values.reduce(:and)
+    Post.where(Post.arel_table[:user_id].eq(id).or(group_posts)).order('created_at DESC')
   end
+
 end
